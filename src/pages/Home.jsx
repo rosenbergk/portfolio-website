5527ff553e92841249ba50302859f24a35e546e7
rosenbergk <ktros49@gmail.com>
2025-05-14 16:25:36 -0400

    Created hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
   return (
@@ -12,7 +13,11 @@ export const Home = () => {
 
       {/* Navbar */}
       <Navbar />
+
       {/* Main Content */}
+      <main>
+        <HeroSection />
+      </main>
 
       {/* Footer */}
     </div>
